69bfa55bb0ddbed55d3f8a6d71079948bc2b119b
huangjp <573155596@qq.com>
2021-07-25 23:31:37 +0800

    在Person类中增加了age属性.....

---
+++ TestModule01/src/com/msb/Person.java
@@ -0,0 +1,11 @@
+package com.msb;
+
+/**
+ * @Auther: HuangJianping
+ * @Date: 2021/7/25 - 07 - 25 - 23:29
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
